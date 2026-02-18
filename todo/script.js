9d417bc06e117a95e0c516b48f303b1da136ba08
Irmina Kalinowska <irminakalinowska0@gmail.com>
2026-02-18 20:15:38 +0100

    todo

--- todo/script.js
+++ todo/script.js
@@ -3,27 +3,49 @@ const ul = document.querySelector('ul');
 const addBtn = document.querySelector('button.add');
 const cleanBtn = document.querySelector('button.clean');
 
+const searchInput = document.querySelector('input.inputsearch');
+const searchButton = document.querySelector('button.search2');
+
 const assignments = [];
 
 const addAssignment = (e) => {
     e.preventDefault();
     if (input.value === '') return;
-    const task = document.createElement('li');
-    task.innerHTML = `<p style="font-size: 16px">*</p>
-                    <p>${input.value}</p>
-                    <button class="remove">X</button>`;
     assignments.push(input.value);
+
     ul.textContent = '';
-    assignments.forEach((assignment) => {
-        ul.appendChild(assignment);
+    assignments.forEach((assignment, key) => {
+        const task = document.createElement('li');
+        task.dataset.key = key;
+        task.innerHTML = `<p style="font-size: 16px">*</p>
+                    <p>${assignment}</p>
+                    <button class="remove">X</button>`;
+
+        task.querySelector('button').addEventListener('click', removeTask);
+
+        ul.appendChild(task);
     });
-    ul.appendChild(task);
     input.value = '';
-    task.querySelector('button').addEventListener('click', removeTask);
+    document.querySelector('span.counter').textContent = assignments.length;
 };
 
 const removeTask = (e) => {
-    e.target.parentNode.remove();
+    const index = e.target.parentNode.dataset.key;
+    assignments.splice(index, 1);
+
+    ul.textContent = '';
+    assignments.forEach((assignment, key) => {
+        const task = document.createElement('li');
+        task.dataset.key = key;
+        task.innerHTML = `<p style="font-size: 16px">*</p>
+                    <p>${assignment}</p>
+                    <button class="remove">X</button>`;
+
+        task.querySelector('button').addEventListener('click', removeTask);
+
+        ul.appendChild(task);
+    });
+    document.querySelector('span.counter').textContent = assignments.length;
 };
 
 addBtn.addEventListener('click', addAssignment);
@@ -36,4 +58,24 @@ cleanBtn.addEventListener('click', (e) => {
     e.preventDefault();
     assignments.length = 0;
     ul.textContent = '';
+    document.querySelector('span.counter').textContent = assignments.length;
+});
+
+searchButton.addEventListener('click', () => {
+    const word = searchInput.value.toLowerCase();
+    ul.textContent = '';
+    console.log(word);
+    const filterArray = assignments.filter((assignment) =>
+        assignment.toLowerCase().includes(word)
+    );
+    console.log(filterArray);
+    console.log(assignments);
+
+    filterArray.forEach((record) => {
+        const task = document.createElement('li');
+        task.innerHTML = `<p style="font-size: 16px">*</p>
+                    <p>${record}</p>`;
+
+        ul.appendChild(task);
+    });
 });
